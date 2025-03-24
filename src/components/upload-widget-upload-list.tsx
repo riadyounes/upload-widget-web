f797f7ba49c394dba19bffc2b62f301d344c42db
Riad Younes <riad.younes@fiscalmax.com.br>
2025-03-24 20:57:37 -0300

    Add immer for state management and update zustand version; refactor upload handling in store

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -2,24 +2,24 @@ import { useUploads } from "../store/uploads";
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
 
 export function UploadWidgetUploadList() {
-  const { uploads } = useUploads();
+	const uploads = useUploads((store) => store.uploads);
 
-  const isUploadListEmpty = uploads.size === 0;
-  return (
-    <div className="px-3 flex flex-col gap-3">
-      <span>
-        Uploaded file <span className="text-zinc-400">({uploads.size})</span>
-      </span>
+	const isUploadListEmpty = uploads.size === 0;
+	return (
+		<div className="px-3 flex flex-col gap-3">
+			<span>
+				Uploaded file <span className="text-zinc-400">({uploads.size})</span>
+			</span>
 
-      {isUploadListEmpty ? (
-        <span className="text-xs text-zinc-400">No uploads added</span>
-      ) : (
-        <div className="flex flex-col gap-2">
-          {Array.from(uploads.entries()).map(([uploadId, upload]) => {
-            return <UploadWidgetUploadItem key={uploadId} upload={upload} />;
-          })}
-        </div>
-      )}
-    </div>
-  );
+			{isUploadListEmpty ? (
+				<span className="text-xs text-zinc-400">No uploads added</span>
+			) : (
+				<div className="flex flex-col gap-2">
+					{Array.from(uploads.entries()).map(([uploadId, upload]) => {
+						return <UploadWidgetUploadItem key={uploadId} upload={upload} />;
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,35 +1,39 @@
+import { enableMapSet } from "immer";
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 export type Upload = {
-  name: string;
-  file: File;
+	name: string;
+	file: File;
 };
 
 type UploadState = {
-  uploads: Map<string, Upload>;
-  addUploads: (files: File[]) => void;
+	uploads: Map<string, Upload>;
+	addUploads: (files: File[]) => void;
 };
 
-export const useUploads = create<UploadState>((set, get) => {
-  function addUploads(files: File[]) {
-    for (const file of files) {
-      const uploadId = crypto.randomUUID();
+enableMapSet();
 
-      const upload: Upload = {
-        name: file.name,
-        file,
-      };
+export const useUploads = create<UploadState, [["zustand/immer", never]]>(
+	immer((set) => {
+		function addUploads(files: File[]) {
+			for (const file of files) {
+				const uploadId = crypto.randomUUID();
 
-      set((state) => {
-        return {
-          uploads: state.uploads.set(uploadId, upload),
-        };
-      });
-    }
-  }
+				const upload: Upload = {
+					name: file.name,
+					file,
+				};
 
-  return {
-    uploads: new Map(),
-    addUploads,
-  };
-});
+				set((state) => {
+					state.uploads.set(uploadId, upload);
+				});
+			}
+		}
+
+		return {
+			uploads: new Map(),
+			addUploads,
+		};
+	}),
+);
