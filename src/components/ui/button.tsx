0985308ed1c1d5867a207620d0676a0204b8a177
Riad Younes <riad.younes@fiscalmax.com.br>
2025-03-25 20:16:36 -0300

    Add Radix UI components for enhanced button and upload list functionality

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,26 +1,32 @@
+import { Slot } from "@radix-ui/react-slot";
 import type { ComponentProps } from "react";
-import { tv, type VariantProps } from "tailwind-variants";
+import { type VariantProps, tv } from "tailwind-variants";
 
 const buttonVariants = tv({
-  base: "text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none",
+	base: "text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none aria-disabled:opacity-50 aria-disabled:pointer-events-none",
 
-  variants: {
-    size: {
-      default: "px-3 py-2",
-      icon: "p-2",
-      "icon-sm": "p-1",
-    },
-  },
+	variants: {
+		size: {
+			default: "px-3 py-2",
+			icon: "p-2",
+			"icon-sm": "p-1",
+		},
+	},
 
-  defaultVariants: {
-    size: "default",
-  },
+	defaultVariants: {
+		size: "default",
+	},
 });
 
-export function Button({
-  size,
-  className,
-  ...props
-}: ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
-  return <button className={buttonVariants({ size, className })} {...props} />;
+type ButtonProps = ComponentProps<"button"> &
+	VariantProps<typeof buttonVariants> & {
+		asChild?: boolean;
+	};
+
+export function Button({ size, className, asChild, ...props }: ButtonProps) {
+	const Component = asChild ? Slot : "button";
+
+	return (
+		<Component className={buttonVariants({ size, className })} {...props} />
+	);
 }
